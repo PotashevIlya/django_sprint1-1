81de5edb706fbcdf810d8666de7cec66ee4b175e
Илья Поташев <potaschev.ilya@yandex.ru>
2024-04-26 15:20:17 +0300

    Версия 1

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -44,15 +44,18 @@ posts = [
     },
 ]
 
+
 def index(request):
     template = 'blog/index.html'
     context = {'post': posts}
     return render(request, template, context)
 
+
 def post_detail(request, pk):
     template = 'blog/detail.html'
     context = {'post': posts[pk]}
     return render(request, template, context)
+
 
 def category_posts(request, category_slug):
     template = 'blog/category.html'
